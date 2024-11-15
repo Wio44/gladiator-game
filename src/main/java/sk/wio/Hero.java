39af5b346cc17a0795973d0a7abfb785143ee6f4
Wio44 <will@zoznam.sk>
2024-11-15 13:11:13 +0100

    Create Hero with name and start the game

---
+++ src/main/java/sk/wio/Hero.java
@@ -0,0 +1,13 @@
+package sk.wio;
+
+public class Hero {
+    private String name;
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/sk/wio/Main.java
@@ -0,0 +1,15 @@
+package sk.wio;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Gladiator game!");
+        System.out.println("Enter your name: ");
+        final String name = scanner.nextLine();
+        final Hero hero = new Hero(name);
+        System.out.println("Hello " + hero.getName() + "! Let's start the game!");
+
+    }
+}
